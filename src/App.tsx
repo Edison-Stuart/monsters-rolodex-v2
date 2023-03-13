d1a55a9eecd03c83b09a4d253559d2e90824e1ac
Edison <edisonstuart@gmail.com>
2023-03-13 17:24:31 -0400

    Migrated react app over to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect, ChangeEvent } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+
+import { getData } from "./utils/data.utils";
+import SearchBox from "./components/search-box/search-box.component";
+
+export type Monster = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState<Monster[]>([]);
+  const [filteredMonsters, setFilteredmonsters] = useState(monsters);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setMonsters(users);
+    };
+
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilteredmonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+        className="monsters-search-box"
+      />
+      <br />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+
+export default App;
